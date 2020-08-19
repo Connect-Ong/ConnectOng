7a384801fe5bc3cc4e246df2a8eca61191f6f4fa
All3yp <noyalle@ualberta.ca>
2020-08-19 17:39:50 -0300

    Add gitignore

---
+++ ConnectOng/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConnectOng
+//
+//  Created by Alley Pereira on 19/08/20.
+//  Copyright © 2020 Alley Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
