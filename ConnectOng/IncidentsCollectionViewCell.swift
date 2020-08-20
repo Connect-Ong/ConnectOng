01e448490f2b145564439738c86e947350513f05
All3yp <noyalle@ualberta.ca>
2020-08-19 21:16:13 -0300

    Created CollectionView and cells

---
+++ ConnectOng/IncidentsCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  IncidentsCollectionViewCell.swift
+//  ConnectOng
+//
+//  Created by Alley Pereira on 19/08/20.
+//  Copyright © 2020 Alley Pereira. All rights reserved.
+//
+
+import UIKit
+
+class IncidentsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var pictureImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ongLabel: UILabel!
+    
+    static let identifier = "IncidentsCollectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.contentView.layer.masksToBounds = false
+        self.applyShadow()
+    }
+
+}
+
+extension UIView {
+    func applyShadow() {
+        //Criando e aplicando a shadow
+        self.layer.cornerRadius = 20.0
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.5, height: 2.0)
+        self.layer.shadowRadius = 3.0
+        self.layer.shadowOpacity = 0.2
+    }
+}
--- ConnectOng/ViewController.swift
+++ ConnectOng/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        self.collectionView.register(UINib(nibName: "IncidentsCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: IncidentsCollectionViewCell.identifier)
     }
-
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IncidentsCollectionViewCell.identifier,
+                                                         for: indexPath) as? IncidentsCollectionViewCell {
+            cell.pictureImageView.backgroundColor = UIColor.random()
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+
+}
+
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor.init(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
+    }
+}
