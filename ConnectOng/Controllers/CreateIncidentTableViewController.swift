072deb45c7a233d5a80b53c29402c5a56a4ed009
All3yp <noyalle@ualberta.ca>
2021-02-12 11:05:24 -0300

    Disable dark mode, support only light mode.

--- ConnectOng/Controllers/CreateIncidentTableViewController.swift
+++ ConnectOng/Controllers/CreateIncidentTableViewController.swift
@@ -43,6 +43,11 @@ class CreateIncidentTableViewController: UITableViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		if #available(iOS 13.0, *) {
+			overrideUserInterfaceStyle = .light
+		} else {
+			// Fallback on earlier versions
+		}
 
 		setupNavigationItem()
 
